6647be5007bb4649c7e7d911e5986bf5267a9562
JesseRM <jesserrm@gmail.com>
2022-04-19 20:54:10 -0700

    Create CompareInstructions component

---
+++ components/CompareInstructions.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/CompareInstructions.module.css";
+
+const CompareInstructions = () => {
+  return (
+    <div className={styles['main-container']}>
+      <p className={styles['message']}>Try searching for and adding candy to compare</p>
+    </div>
+  )
+}
+
+export default CompareInstructions;
