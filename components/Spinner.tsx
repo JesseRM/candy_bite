a2066a1d4a3ac41a860e9780fb96710a474ae3c6
JesseRM <jesserrm@gmail.com>
2022-04-19 23:26:58 -0700

    Create Spinner component

---
+++ components/Spinner.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Spinner.module.css";
+
+const Spinner = () => {
+  return (
+    <div className={styles['main-container']}>
+      <div className={styles['spinner']}></div>
+    </div>
+  )
+}
+
+export default Spinner;
