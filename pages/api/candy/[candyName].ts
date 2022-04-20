6d3f836919c2b69fafe5163f31ac6a887b8c2754
JesseRM <jesserrm@gmail.com>
2022-04-20 15:51:59 -0700

    Create API to retrieve candy information

---
+++ pages/api/candy/[candyName].ts
@@ -0,0 +1,62 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../prisma/client';
+
+interface CandyInfo {
+  candyName: String;
+  imageUrl: String;
+  portion: number;
+  nutrients: Object[]
+}
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const candyName: string | undefined = String(req.query.candyName);
+
+  try {
+    const candies = await prisma.candy.findMany({
+      where: {
+        candy_name : {
+          contains: candyName
+        }
+      }
+    });
+
+    const fdcIds = [];
+
+    for (const candy of candies) {
+      fdcIds.push(candy.fdc_id);
+    }
+
+    const usda_api = `https://api.nal.usda.gov/fdc/v1/foods?fdcIds=${fdcIds.join()}&api_key=${process.env.USDA_API_KEY}`;
+
+    const data: CandyInfo[] = [];
+
+    const results = await fetch(usda_api).then(res => res.json());
+
+    for (let i = 0; i < results.length; i++) {
+      const current: any = results[i];
+
+      const candyName = candies[i].candy_name;
+      const imageUrl = candies[i].image_url;
+      const portion = current['inputFoods'][0]['ingredientWeight'];
+      const nutrients = current['foodNutrients'];
+      
+      
+      const info: CandyInfo = {
+        candyName,
+        imageUrl,
+        portion,
+        nutrients
+      }
+
+      data.push(info);
+    }
+    
+    res.status(200);
+    res.json(data);
+  } catch (error) {
+    res.status(500);
+    res.json({error: "Unable to retreive information from db"})
+  }
+}
+
+export default handler;
---
+++ pages/api/candy/index.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../prisma/client';
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const candies = await prisma.candy.findMany();
+
+    res.status(200);
+    res.json(candies);
+  } catch (error) {
+    res.status(500);
+    res.json({error: "Unable to retreive information from db"})
+  } 
+}
+
+export default handler;
