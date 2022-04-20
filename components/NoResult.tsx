a819d4fda1c38a8f86a58a359248dc51ee66bd70
JesseRM <jesserrm@gmail.com>
2022-04-19 22:27:39 -0700

    Create NoResult component

---
+++ components/NoResult.tsx
@@ -0,0 +1,13 @@
+import styles from "../styles/NoResult.module.css";
+
+const NoResult = () => {
+  return (
+    <div className={styles['main-container']}>
+      <p className={styles['message']}>
+        Sorry, no results matching that term.  Please try a different term.
+      </p>
+    </div>
+  )
+}
+
+export default NoResult;
