242b3ef5dd2fed9e7264d8ba35cf567cad7dc42f
JesseRM <jesserrm@gmail.com>
2022-04-20 16:07:11 -0700

    Add context provider and layout component to app entry point

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CandyBiteProvider } from '../context/state'
+import Layout from '../components/Layout'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CandyBiteProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CandyBiteProvider>
+  ) 
 }
 
 export default MyApp
