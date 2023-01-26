5f7586f21c9b1478a5d03fea2c96d644eb672099
JesseRM <jesserrm@gmail.com>
2023-01-25 22:40:23 -0800

    SearchBar now takes in a searchHandler function which executes when a search is attempted

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,62 +1,24 @@
-import {
-  Dispatch,
-  SetStateAction,
-  useContext,
-  useEffect,
-  useState,
-} from "react";
+import { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import CandyBiteContext from "../context/state";
-import { CandyInfo } from "../interfaces/globalInterfaces";
 import styles from "../styles/SearchBar.module.css";
 
 interface SearchBarProps {
-  setSearchResults: Dispatch<SetStateAction<CandyInfo[]>>;
-  setFetching: Dispatch<SetStateAction<boolean>>;
-  setNoResults: Dispatch<SetStateAction<boolean>>;
-  setDisplayErrorMessage: Dispatch<SetStateAction<boolean>>;
+  searchHandler: (s: string) => void;
 }
 
-const SearchBar = ({
-  setSearchResults,
-  setFetching,
-  setNoResults,
-  setDisplayErrorMessage,
-}: SearchBarProps) => {
+const SearchBar = ({ searchHandler }: SearchBarProps) => {
   const [term, setTerm] = useState("");
-  const { setAttemptedSearch } = useContext(CandyBiteContext);
 
   function handleclick() {
-    fetchCandy();
+    searchHandler(term);
   }
 
   function handleKeyUp(event: any) {
     const ENTER_KEY: number = 13;
 
     if (event.keyCode === ENTER_KEY) {
-      fetchCandy();
+      searchHandler(term);
     }
-  }
-
-  function fetchCandy() {
-    if (!term || term.trim().length === 0) return;
-    
-    setAttemptedSearch(true);
-    setFetching(true);
-
-    const url = `/api/candy/${term.toLowerCase()}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setFetching(false);
-        setSearchResults(data);
-        setNoResults(data.length === 0 ? true : false);
-      })
-      .catch((error) => {
-        console.log(error);
-        setFetching(false);
-        setDisplayErrorMessage(true);
-      });
   }
 
   //Clear input element on mount
@@ -71,7 +33,7 @@ const SearchBar = ({
       <input
         className={styles["search-input"]}
         type="search"
-        placeholder="Search candy"
+        placeholder="     Search candy"
         onChange={(event) => setTerm(event.target.value)}
         onKeyUp={handleKeyUp}
       />
