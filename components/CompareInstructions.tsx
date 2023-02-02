4cd5e0aa173cd183f01ab2d46128322b423c5f33
JesseRM <jesserrm@gmail.com>
2023-02-01 21:39:19 -0800

    Use add circle for clarity

--- components/CompareInstructions.tsx
+++ components/CompareInstructions.tsx
@@ -1,10 +1,11 @@
 import styles from "../styles/CompareInstructions.module.css";
+import { IoAddCircle } from "react-icons/io5";
 
 const CompareInstructions = () => {
   return (
     <div className={styles["main-container"]}>
       <p className={styles["message"]}>
-        Try searching for and adding candy to compare
+        Try searching for and adding <IoAddCircle /> candy to compare
       </p>
     </div>
   );
