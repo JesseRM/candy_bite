6a6e07bff924a7684baaee288de85c6226c01799
JesseRM <jesserrm@gmail.com>
2023-03-08 21:46:09 -0800

    Refactor to send single GET request to APi with multiple FDC IDs

--- pages/api/candy/[candyName].ts
+++ pages/api/candy/[candyName].ts
@@ -6,6 +6,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   const candyName: string | undefined = String(req.query.candyName);
 
   try {
+    //Fetch candy info from database
     const candies = await prisma.candies.findMany({
       where: {
         candy_name: {
@@ -17,6 +18,7 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       },
     });
 
+    //Fetch nutritional data from USDA API for each hit returned from database
     const data: CandyInfo[] = [];
     /*
         208 - Calories
@@ -27,12 +29,31 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
         307 - Sodium
     */
     const nutrientCodes: number[] = [208, 204, 269, 203, 205, 307];
+    const fdcIds: string[] = [];
 
     for (const candy of candies) {
-      const fdcId = candy.fdc_id;
-      const usdaApi = `https://api.nal.usda.gov/fdc/v1/food/${fdcId}?nutrients=${nutrientCodes.join()}&api_key=${
+      fdcIds.push(candy.fdc_id);
+    }
+
+    while (fdcIds.length) {
+      const fdcIdsToFetch: string[] = [];
+      const LIMIT = 20; //USDA API allows up to 20 FDC ID searches per request
+
+      //Extract FDC IDs to insert into API URL
+      for (let i = 0; i < LIMIT; i++) {
+        const fdcId = fdcIds.pop();
+
+        if (fdcId !== undefined) {
+          fdcIdsToFetch.push(fdcId);
+        } else {
+          break;
+        }
+      }
+
+      const usdaApi = `https://api.nal.usda.gov/fdc/v1/foods?fdcIds=${fdcIdsToFetch.join()}&nutrients=${nutrientCodes.join()}&api_key=${
         process.env.USDA_API_KEY
       }`;
+
       let response: Response | null = null;
       let result: any;
 
@@ -63,22 +84,32 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       if (response) {
         result = await response.json();
 
-        const candyName = candy.candy_name;
-        const imageUrl = candy.candy_images[0].image_url;
-        const portion: number = 100; //Default is 100 grams
-        const nutrients = result["foodNutrients"].sort(
-          (a: any, b: any) =>
-            Number(a["nutrient"]["number"]) - Number(b["nutrient"]["number"])
-        );
+        for (const candy of candies) {
+          const currentFdcId = candy.fdc_id;
+          const candyName = candy.candy_name;
+          const imageUrl = candy.candy_images[0].image_url;
+          const portion: number = 100; //Default is 100 grams
+          const candyInfo = result.find(
+            ({ fdcId }: { fdcId: number }) => String(fdcId) === currentFdcId
+          );
 
-        const info: CandyInfo = {
-          candyName,
-          imageUrl,
-          portion,
-          nutrients,
-        };
+          if (candyInfo) {
+            const nutrients = candyInfo["foodNutrients"].sort(
+              (a: any, b: any) =>
+                Number(a["nutrient"]["number"]) -
+                Number(b["nutrient"]["number"])
+            );
 
-        data.push(info);
+            const info: CandyInfo = {
+              candyName,
+              imageUrl,
+              portion,
+              nutrients,
+            };
+
+            data.push(info);
+          }
+        }
       }
     }
 
