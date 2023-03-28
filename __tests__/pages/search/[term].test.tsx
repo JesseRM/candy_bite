b70438babd0dde73e3f0bc012149ca01df049597
JesseRM <jesserrm@gmail.com>
2023-03-27 22:29:46 -0700

    Use msw instead of mockin fetch, modify tests

--- __tests__/pages/search/[term].test.tsx
+++ __tests__/pages/search/[term].test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { CandyBiteProvider } from "@/context/state";
 import { useRouter } from "next/router";
 import React from "react";
@@ -8,82 +8,9 @@ jest.mock("next/router", () => ({
   useRouter: jest.fn(),
 }));
 
-const mockedResponse = {
-  candyName: "snickers",
-  imageUrl: "https://i.imgur.com/GA8grvo.png",
-  portion: "",
-  nutrients: {},
-};
-
-(global.fetch as jest.Mock) = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve(mockedResponse),
-    status: 200,
-  })
-);
-
 describe("Search page with results", () => {
-  test("Url change and api call with term on search button click", () => {
-    const push = jest.fn();
-    const urlTerm = "test";
-    (useRouter as jest.Mock).mockImplementation(() => ({
-      push,
-      query: { urlTerm },
-      isReady: true,
-    }));
-
-    render(
-      <CandyBiteProvider>
-        <Search />
-      </CandyBiteProvider>
-    );
-
-    const searchbox = screen.getByRole("searchbox");
-    const button = screen.getByRole("button");
-    const newTerm = "snickers";
-
-    fireEvent.change(searchbox, { target: { value: newTerm } });
-    fireEvent.click(button);
-
-    expect(push).toHaveBeenCalledWith(`/search/${newTerm}`);
-
-    const apiUrl = "/api/candy/" + newTerm;
-    expect(global.fetch).toHaveBeenCalledWith(apiUrl);
-  });
-
-  test("URL change with term on pressing Enter key", () => {
-    const push = jest.fn();
-    const term = "test";
-    (useRouter as jest.Mock).mockImplementation(() => ({
-      push,
-      query: { term },
-      isReady: true,
-    }));
-
-    render(
-      <CandyBiteProvider>
-        <Search />
-      </CandyBiteProvider>
-    );
-
-    const searchbox = screen.getByRole("searchbox");
-    const newTerm = "twix";
-
-    fireEvent.change(searchbox, { target: { value: newTerm } });
-    fireEvent.keyDown(searchbox, {
-      key: "Enter",
-      code: "Enter",
-      charCode: 13,
-    });
-
-    expect(push).toHaveBeenCalledWith(`/search/${newTerm}`);
-
-    const apiUrl = "/api/candy/" + newTerm;
-    expect(global.fetch).toHaveBeenCalledWith(apiUrl);
-  });
-
   test("Contains h1 with search term", () => {
-    const term = "hershey";
+    const term = "snickers";
     (useRouter as jest.Mock).mockImplementation(() => ({
       query: { term },
       isReady: true,
@@ -98,17 +25,25 @@ describe("Search page with results", () => {
     expect(screen.getByRole("heading")).toHaveTextContent(term);
   });
 
-  test("Contains image of searched candy", () => {
+  test("Contains searchbox and search button", () => {
+    render(
+      <CandyBiteProvider>
+        <Search />
+      </CandyBiteProvider>
+    );
+
+    const searchbox = screen.getByRole("searchbox");
+    expect(searchbox).toBeInTheDocument();
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+
+  test("Contains image of searched candy", async () => {
     const term = "snickers";
     (useRouter as jest.Mock).mockImplementation(() => ({
       query: { term },
       isReady: true,
     }));
-
-    const mockUseContext = jest.spyOn(React, "useContext");
-    mockUseContext.mockReturnValue({
-      searchResults: [mockedResponse],
-    });
 
     const altText = term + " image";
 
@@ -118,6 +53,8 @@ describe("Search page with results", () => {
       </CandyBiteProvider>
     );
 
-    expect(screen.getByAltText(altText)).toBeInTheDocument();
+    await waitFor(() =>
+      expect(screen.getByAltText(altText)).toBeInTheDocument()
+    );
   });
 });
