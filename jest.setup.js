2fdecd0c5830e0cf55ba62f0c455ee9d8f998f05
JesseRM <jesserrm@gmail.com>
2023-03-27 22:28:57 -0700

    Import fetch polyfill and start msw server before tests

--- jest.setup.js
+++ jest.setup.js
@@ -3,4 +3,19 @@
 
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
+import "whatwg-fetch";
 import "@testing-library/jest-dom/extend-expect";
+
+import { server } from "./mocks/server";
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
