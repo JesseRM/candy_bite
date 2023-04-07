f50aa94fe665911550c739aff137cd5fb018f85f
JesseRM <jesserrm@gmail.com>
2023-04-06 23:58:24 -0700

    Move interfaces for Search page to own module

---
+++ interfaces/searchPageInterfaces.ts
@@ -0,0 +1,13 @@
+export interface NutrientState {
+  amountPer100: number;
+  amount: number;
+}
+
+export interface UnitNames {
+  protein: string;
+  fat: string;
+  carbohydrate: string;
+  calories: string;
+  sugar: string;
+  sodium: string;
+}
