86e0097f14d936b100887a5bc5ae68fa1d6af6e8
JesseRM <jesserrm@gmail.com>
2023-04-23 21:54:31 -0700

    Use full candy mock

---
+++ mocks/candy.ts
@@ -0,0 +1,126 @@
+export const snickers = {
+  candyName: "snickers",
+  fdcId: "169589",
+  imageUrl: "https://i.imgur.com/GA8grvo.png",
+  portion: 100,
+  nutrients: [
+    {
+      type: "FoodNutrient",
+      nutrient: {
+        id: 1003,
+        number: "203",
+        name: "Protein",
+        rank: 600,
+        unitName: "g",
+      },
+      foodNutrientDerivation: {
+        id: 47,
+        code: "MC",
+        description:
+          "Manufacturer supplied; Calculated by manufacturer or unknown if analytical or calculated",
+        foodNutrientSource: [Object],
+      },
+      id: 1455177,
+      amount: 7.53,
+      dataPoints: 1,
+    },
+    {
+      type: "FoodNutrient",
+      nutrient: {
+        id: 1004,
+        number: "204",
+        name: "Total lipid (fat)",
+        rank: 800,
+        unitName: "g",
+      },
+      foodNutrientDerivation: {
+        id: 47,
+        code: "MC",
+        description:
+          "Manufacturer supplied; Calculated by manufacturer or unknown if analytical or calculated",
+        foodNutrientSource: [Object],
+      },
+      id: 1455197,
+      amount: 23.85,
+      dataPoints: 1,
+    },
+    {
+      type: "FoodNutrient",
+      nutrient: {
+        id: 1005,
+        number: "205",
+        name: "Carbohydrate, by difference",
+        rank: 1110,
+        unitName: "g",
+      },
+      foodNutrientDerivation: {
+        id: 49,
+        code: "NC",
+        description: "Calculated",
+        foodNutrientSource: [Object],
+      },
+      id: 1455218,
+      amount: 61.51,
+      dataPoints: 0,
+    },
+    {
+      type: "FoodNutrient",
+      nutrient: {
+        id: 1008,
+        number: "208",
+        name: "Energy",
+        rank: 300,
+        unitName: "kcal",
+      },
+      foodNutrientDerivation: {
+        id: 49,
+        code: "NC",
+        description: "Calculated",
+        foodNutrientSource: [Object],
+      },
+      id: 1455219,
+      amount: 491,
+      dataPoints: 0,
+    },
+    {
+      type: "FoodNutrient",
+      nutrient: {
+        id: 2000,
+        number: "269",
+        name: "Sugars, total including NLEA",
+        rank: 1510,
+        unitName: "g",
+      },
+      foodNutrientDerivation: {
+        id: 47,
+        code: "MC",
+        description:
+          "Manufacturer supplied; Calculated by manufacturer or unknown if analytical or calculated",
+        foodNutrientSource: [Object],
+      },
+      id: 1455217,
+      amount: 50.47,
+      dataPoints: 1,
+    },
+    {
+      type: "FoodNutrient",
+      nutrient: {
+        id: 1093,
+        number: "307",
+        name: "Sodium, Na",
+        rank: 5800,
+        unitName: "mg",
+      },
+      foodNutrientDerivation: {
+        id: 44,
+        code: "LC",
+        description:
+          "Label claim (back calculated from label by NDL staff; Calculated from label claim/serving (g or %RDI)",
+        foodNutrientSource: [Object],
+      },
+      id: 1455220,
+      amount: 239,
+      dataPoints: 0,
+    },
+  ],
+};
--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,18 +1,8 @@
 import { rest } from "msw";
-import { snickerNutrients } from "./nutrients";
+import { snickers } from "./candy";
 
 export const handlers = [
   rest.get("/api/candy/snickers", (req, res, ctx) => {
-    return res(
-      ctx.json([
-        {
-          candyName: "snickers",
-          fdcId: "169589",
-          imageUrl: "https://i.imgur.com/GA8grvo.png",
-          portion: 100,
-          nutrients: snickerNutrients,
-        },
-      ])
-    );
+    return res(ctx.json([snickers]));
   }),
 ];
